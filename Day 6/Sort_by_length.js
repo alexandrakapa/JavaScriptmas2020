aa4525951deb2e25341165f4446edc2640bf71f7
alexandrakapa <63066416+alexandrakapa@users.noreply.github.com>
2020-12-22 23:07:37 +0200

    Create Sort_by_length.js

---
+++ Day 6/Sort_by_length.js
@@ -0,0 +1,26 @@
+function sortByLength(strs) {
+    //  write code here.
+    var y = strs.sort(function(a,b){return a.length-b.length});
+    return y;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('sortByLength()', () => {
+    it('sorts the strings from shortest to longest', () => {
+        // arrange
+        const strs = ["abc", "", "aaa", "a", "zz"];
+        
+        // act
+        const result = sortByLength(strs);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
+    });
+});
